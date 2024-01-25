87f231547394909a4e9e2ccf6956d9e3c15ae838
Sadel Muwahed <sadeel.mu15@gmail.com>
2024-01-25 20:36:59 +0300

    Calculator created using PyQT6

---
+++ README.md
@@ -0,0 +1,38 @@
+# PyCalc
+
+PyCalc is a sample calculator implemented using Python 3 and with a [PyQt6](https://www.riverbankcomputing.com/static/Docs/PyQt6/introduction.html) GUI. PyCalc implements basic math operations, including division, multiplication, addition, and subtraction.
+
+PyCalc is intended to be a demonstrative example of how to build a Python + PyQt6 GUI application using the Model-View-Controller (MVC) pattern.
+
+## Screenshot
+
+![Screenshot](screenshot.png)
+
+## Requirements
+
+For PyCalc to work, you need to have [Python](https://www.python.org) >= 3.6.1. Then you need to install the PyQt6 library. You can do this by using `pip` in a Python virtual environment:
+
+```console
+$ python -m venv venv
+$ source venv/bin/activate
+$ python -m pip install pyqt6
+```
+
+After these commands have finished, you can run PyCalc as described in the next section.
+
+## How to Run PyCalc
+
+To run PyCalc from your system's command line or terminal, execute the following command:
+
+```console
+$ python pycalc.py
+```
+
+After running this command, you'll get PyCalc running on your machine.
+
+## How to Use PyCalc
+
+To use PyCalc, just enter a valid math expression using your mouse and then press `Enter` or click the `=` button to get the result:
+
+![Screenshot](pycalc-howto.gif)
+
---
+++ pycalc.py
@@ -0,0 +1,124 @@
+"""PyCalc is a simple calculator built with Python and PyQt."""
+
+import sys
+from functools import partial
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+ERROR_MSG = "ERROR"
+WINDOW_SIZE = 250
+DISPLAY_HEIGHT = 50
+BUTTON_SIZE = 40
+
+
+class PyCalcWindow(QMainWindow):
+    """PyCalc's main window (GUI or view)."""
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyCalc")
+        self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.generalLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self._createDisplay()
+        self._createButtons()
+
+    def _createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(DISPLAY_HEIGHT)
+        self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
+
+    def _createButtons(self):
+        self.buttonMap = {}
+        buttonsLayout = QGridLayout()
+        keyBoard = [
+            ["7", "8", "9", "/", "C"],
+            ["4", "5", "6", "*", "("],
+            ["1", "2", "3", "-", ")"],
+            ["0", "00", ".", "+", "="],
+        ]
+
+        for row, keys in enumerate(keyBoard):
+            for col, key in enumerate(keys):
+                self.buttonMap[key] = QPushButton(key)
+                self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                buttonsLayout.addWidget(self.buttonMap[key], row, col)
+
+        self.generalLayout.addLayout(buttonsLayout)
+
+    def setDisplayText(self, text):
+        """.setDisplayText() uses .setText() to set and update the display’s text. It also uses .setFocus() to set the cursor’s focus on the display."""
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def displayText(self):
+        """a getter method that returns the display’s current text. When the user clicks the equal sign (=) on the calculator’s keyboard, the app will use the return value of .displayText() as the math expression to be evaluated."""
+        return self.display.text()
+
+    def clearDisplay(self):
+        """sets the display’s text to an empty string ("") so that the user can introduce a new math expression. This method will be triggered every time the user presses the C button on the calculator’s board."""
+        self.setDisplayText("")
+
+
+def evaluateExpression(expression):
+    """Evaluate an expression (Model)."""
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+    return result
+
+
+class PyCalc:
+    """PyCalc's controller class."""
+
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText() + subExpression
+        self._view.setDisplayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.items():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(
+                    partial(self._buildExpression, keySymbol)
+                )
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
+
+
+def main():
+    """PyCalc's main function."""
+    pycalcApp = QApplication([])
+    pycalcWindow = PyCalcWindow()
+    pycalcWindow.show()
+    PyCalc(model=evaluateExpression, view=pycalcWindow)
+    sys.exit(pycalcApp.exec())
+
+
+if __name__ == "__main__":
+    main()
